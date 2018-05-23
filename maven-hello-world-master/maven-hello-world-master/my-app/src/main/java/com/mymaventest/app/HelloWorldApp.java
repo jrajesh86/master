f9cea15021502e04af1a20c7f760052ebfb65c46
jrajesh86 <mail2jrajesh@gmail.com>
2018-05-23 11:20:47 +0530

    Add files via upload

---
+++ maven-hello-world-master/maven-hello-world-master/my-app/src/main/java/com/mymaventest/app/HelloWorldApp.java
@@ -0,0 +1,13 @@
+package com.mymaventest.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorldApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
